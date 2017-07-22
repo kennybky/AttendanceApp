20599db24b7d4014eac1a34fd3e63a1bb4b75d96
kennybky <kennybky92@gmail.com>
2017-07-22 08:31:39 -0700

    Update Files

---
+++ AttendanceApp/app/src/main/java/com/jkva/android/attendanceapp/AddPersonFragment.java
@@ -0,0 +1,90 @@
+package com.jkva.android.attendanceapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.microsoft.projectoxford.face.*;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by kenny on 7/22/2017.
+ */
+
+public class AddPersonFragment extends DialogFragment {
+
+    private static final int REQUEST_TAKE_PHOTO = 0 ;
+    private static final int REQUEST_SELECT_IMAGE_IN_ALBUM = 1;
+    private FragmentActivity activity;
+    private Uri mUriPhotoTaken;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_person_fragement, container, false);
+            activity = getActivity();
+        return null;
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode)
+        {
+            case REQUEST_TAKE_PHOTO:
+            case REQUEST_SELECT_IMAGE_IN_ALBUM:
+                if (resultCode == activity.RESULT_OK) {
+                    Uri imageUri;
+                    if (data == null || data.getData() == null) {
+                        imageUri = mUriPhotoTaken;
+                    } else {
+                        imageUri = data.getData();
+                    }
+                    Intent intent = new Intent();
+                    intent.setData(imageUri);
+                    activity.setResult(activity.RESULT_OK, intent);
+                    activity.finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void takePhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(activity.getPackageManager()) != null) {
+            // Save the photo taken to a temporary file.
+            File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                mUriPhotoTaken = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mUriPhotoTaken);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    // When the button of "Select a Photo in Album" is pressed.
+    public void selectImageInAlbum(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        if (intent.resolveActivity(activity.getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM);
+        }
+    }
+}
--- AttendanceApp/app/src/main/java/com/jkva/android/attendanceapp/PersonGroupActivity.java
+++ AttendanceApp/app/src/main/java/com/jkva/android/attendanceapp/PersonGroupActivity.java
@@ -1,7 +1,10 @@
 package com.jkva.android.attendanceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by kenny on 7/18/2017.
@@ -21,5 +24,17 @@ public class PersonGroupActivity extends AppCompatActivity {
                 groupName = bundle.getString("personGroupName");
         }
     }
+    //Adds person
+    public void addPerson(View view) {
+        FragmentManager fm = getSupportFragmentManager();
+        AddPersonFragment newFragment = new AddPersonFragment();
+        newFragment.show(fm, "tag");
+    }
 
+    public void takeAttendance(View view) {
+
+    }
+
+    public void viewHistory(View view) {
+    }
 }
